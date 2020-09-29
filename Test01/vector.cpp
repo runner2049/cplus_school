f049c107e679a1d31470e24d15fd7a8076e49a5b
runner2049 <vbprv@yandex.com>
2020-09-29 11:52:32 +0300

    null

---
+++ Test01/vector.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+struct vector{
+
+    double x, y;
+    
+};
+
+double R(vector.x, vector.y ){
+        
+        return sqrt(x*x + y*y);
+    }
+
+
+int main(){
+
+}
